846f7e8b7ddc84746103a714fb43ad76a7c60cc5
Alve576 <shadmanzz656@gmail.com>
2022-01-01 20:46:00 +0600

    'backend-server-build'

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require ("express");
 const  app = express();
-const port = process.env.port || 5000;
 const cors = require('cors');
 const {MongoClient, ObjectId} = require('mongodb');
+const port = process.env.PORT || 5000;
 
 
 require('dotenv').config();
